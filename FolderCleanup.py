73321a15e64e8d306737a6ca7718cda17fc062c1
Cessor <github@spam.cessor.de>
2012-07-02 14:00:58 +0200

    Added more extensions to be cleaned up

--- FolderCleanup.py
+++ FolderCleanup.py
@@ -46,16 +46,16 @@ class Log:
 
 class DirectoryStructure:
 	map = dict(
-		Archives = ["zip", "rar", "7z", "tar", "gz", "bz2"],
+		Archives = ["zip", "rar", "7z", "tar", "gz", "bz2", "tgz"],
 		Binaries = ["exe", "dll", "msi", "vsix", "xpi"],
 		Books = ["epub", "mobi"],
-		Documents = ["doc", "docx", "txt", "pdf", "xps", "dotx"],
+		Documents = ["doc", "docx", "txt", "pdf", "xps", "dotx", "odt", "html"],
 		Images = ["iso","mkv","mdf"],
 		Movies = ["avi","mpg","mpeg","mov","mp4", "flv"],
 		Music = ["mp3","wav", "ogg", "wma"],
 		Pictures = ["jpg","png","jpeg", "tif", "gif"],
 		Presentations=["ppt", "pptx"],
-		Sourcecode = ["cs","css","cpp","h","c", "js", "php", "py", "java", "pyc"],
+		Sourcecode = ["cs","css","cpp","h","c", "js", "php", "py", "java", "pyc", "xaml", "xml", "rb", "config"],
 		Spreadsheets = ["csv","xlsx", "xls", "json"]
 	)
 
